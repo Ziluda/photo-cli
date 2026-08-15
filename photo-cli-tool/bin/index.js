ace491c29a9b6c000701acf1bfc4b3a9cf233f41
Zil Pavdighada <zil.pavdigh@gmail.com>
2026-08-14 21:08:30 -0400

    Added size functionality

--- photo-cli-tool/bin/index.js
+++ photo-cli-tool/bin/index.js
@@ -2,14 +2,16 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const inputPath = "../../../Desktop/messy";
-const emptyPath = "../../../Desktop/messy/thisEmpty";
 
-function deepest(folderPath) {
+function direntArray(folderPath) {
     // create the array of all the items (files AND/OR folders) in this directory using folderPath
-    const folderArray = fs.readdirSync(folderPath, { withFileTypes: true });
+    // returns a list of Dirent objects
+    return fs.readdirSync(folderPath, { withFileTypes: true });
+}
+function deepest(folderPath) {
 
     // create the an array with only directories
-    const directories = folderArray.filter((item) => item.isDirectory());
+    const directories = direntArray(folderPath).filter((item) => item.isDirectory());
 
     // base case: when you reach a leaf folder
     // check if this directory is a leaf folder (i.e. there shouldn't be any directories in it)
@@ -23,8 +25,26 @@ function deepest(folderPath) {
 }
 
 function empty(folderPath) {
-    const folderArray = fs.readdirSync(folderPath, { withFileTypes: true });
-    return (folderArray.length === 0);
+    return (direntArray(folderPath).length === 0);
 }
 
-console.log(`deepest: ${deepest(inputPath)}\nempty true: ${empty(emptyPath)}`);
+function itemSize(itemPath) {
+    return fs.statSync(itemPath).size;
+}
+
+function folderSize(folderPath) {
+    const folderArray = direntArray(folderPath);
+    let totalBytes = 0;
+
+    for (const item of folderArray) {
+        itemPath = path.join(folderPath, item.name);
+
+        if (item.isDirectory()) {
+            totalBytes += folderSize(itemPath);
+        } else if (item.isFile()) {
+            totalBytes += itemSize(itemPath);
+        }
+    }
+
+    return totalBytes;
+}
