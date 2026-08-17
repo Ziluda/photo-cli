00bfc8f7da51cc37e2ad3a8fba7cb17df4029d8f
Zil Pavdighada <zil.pavdigh@gmail.com>
2026-08-16 21:29:34 -0400

    Updated deepest and implemented empty counter

--- photo-cli-tool/bin/index.js
+++ photo-cli-tool/bin/index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const inputPath = "../../../Desktop/messy";
+const che = `../../../Desktop/mine/family/Chetan`;
 
 function direntArray(folderPath) {
     // create the array of all the items (files AND/OR folders) in this directory using folderPath
@@ -16,16 +17,52 @@ function deepest(folderPath) {
     // base case: when you reach a leaf folder
     // check if this directory is a leaf folder (i.e. there shouldn't be any directories in it)
     if (directories.length === 0) {
-        return 0;
+        return [0, folderPath];
+    }
+
+    let maxDepth = 0;
+    let maxPath = folderPath;
+
+    for (const directory of directories) {
+        const currentPath = path.join(folderPath, directory.name);
+        const currentResult = deepest(currentPath);
+        const currentDepth = 1 + currentResult[0];
+
+        if (currentDepth>maxDepth) {
+            maxDepth = currentDepth;
+            maxPath = currentResult[1];
+        }
+
     }
 
     // recursive case: when there is at least one directory in the next level
-    return 1 + Math.max(...(directories.map(item => deepest(path.join(folderPath, item.name)))));
+    return [maxDepth, maxPath];
+
+    // shorter way
+    // return 1 + Math.max(...(directories.map(item => deepest(path.join(folderPath, item.name)))));
 
 }
-
 function empty(folderPath) {
+    // creates and returns an array of Dirent objects (files and folders)
     return (direntArray(folderPath).length === 0);
+}
+
+function countEmpty(folderPath) {
+
+    if (empty(folderPath)) {
+        return 1;
+    }
+
+    const folderArray = direntArray(folderPath);
+
+    let counter = 0;
+
+    for (const item of folderArray) {
+        if (item.isDirectory()) {
+            counter += countEmpty(path.join(folderPath, item.name));
+        }
+    }
+    return counter;
 }
 
 function itemSize(itemPath) {
